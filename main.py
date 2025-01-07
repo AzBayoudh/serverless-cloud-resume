d76521ebcba30dc68464953103520af8aed752cf
AzBayoudh <azbayoudh1@gmail.com>
2025-01-07 14:44:45 -0600

    decimal error debug

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CloudResumeTest')
 
 # Lambda handler function
-def lambda_handler(event, context):
+def handler(event, context):
     try:
         # Get the item from DynamoDB
         response = table.get_item(Key={'id': '1'})
 
         # Check if the item exists
         if 'Item' in response:
-            views = response['Item'].get('views', 0)  # Default to 0 if 'views' doesn't exist
+            views = response['Item'].get('views')  # Default to 0 if 'views' doesn't exist
         else:
             # Initialize the item if it doesn't exist
             views = 0
